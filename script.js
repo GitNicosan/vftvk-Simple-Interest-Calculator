d7aa2ca335a92428a392a0da4b27af32ecfefccd
GitNicosan <87877325+GitNicosan@users.noreply.github.com>
2021-08-05 16:43:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function compute()
     var realYears = years - 1
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(realYears);
+
+    if (principal <= 0) {
+        alert("Enter a positive number");
+    }
+
     document.getElementById("result").innerHTML = "If you deposit: " + principal + "<br/>" +
         "at an interest rate of: " + rate + "<br/>" +
         "You will receive an amount of: " + interest + "<br/> " +
@@ -17,4 +22,7 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-     
+
+focusMethod = function getFocus() {
+    document.getElementById("principal").focus();
+}   
